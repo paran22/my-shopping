6157992a4f0fd8e186a473878f1afbdb21753c68
gaeun <knospe1@naver.com>
2023-08-24 08:26:48 +0900

    Feat: Add UserContext

--- src/components/navbar/LoginButton.jsx
+++ src/components/navbar/LoginButton.jsx
@@ -1,28 +1,11 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import {
-  loginByGoogle,
-  logoutByGoogle,
-  onUserStateChange,
-} from "../../auth/googleAuthProvider";
+import { useUserContext } from "../../context/UserContext";
 import { NavTextItem } from "./NavItem";
 
 export default function LoginButton() {
-  const [user, setUser] = useState(undefined);
-  useEffect(() => onUserStateChange(setUser), []);
-  const isLogin = user !== null;
-
-  const login = async () => {
-    const user = await loginByGoogle();
-    setUser(user);
-  };
-  const logout = async () => {
-    const result = await logoutByGoogle();
-    setUser(result);
-  };
+  const { isLogin, login, logout, isAuthInfoLoading } = useUserContext();
   return (
-    <div className={user === undefined ? "invisible" : "visible"}>
+    <div className={isAuthInfoLoading ? "invisible" : "visible"}>
       <NavTextItem
         name={isLogin ? "Logout" : "Login"}
         onClick={isLogin ? logout : login}
