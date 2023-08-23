fccece13c3a4f0f007c6440ca4261c9abda3e12a
gaeun <knospe1@naver.com>
2023-08-23 14:53:05 +0900

    Feat: add pages and router

---
+++ src/router/router.js
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import HomePage from '../Page/HomePage';
+import NotFoundPage from '../Page/NotFoundPage';
+import ProductsPage from '../Page/ProductsPage';
+import ProductDetailPage from '../Page/ProductDetailPage';
+import LoginPage from '../Page/LoginPage';
+import ProductAddPage from '../Page/ProductAddPage';
+
+export default function getRouter() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />
+        },
+        {
+          path: '/products',
+          element: <ProductsPage />
+        },
+        {
+          path: '/products/:productId',
+          element: <ProductDetailPage />
+        },
+        {
+          path: '/login',
+          element: <LoginPage />
+        },
+        {
+          path: '/products/add',
+          element: <ProductAddPage />
+        }
+      ],
+    }
+  ]);
+  return router;
+}
