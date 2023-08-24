3fc53882d4412c86cee59a6ac41acb61833d5601
gaeun <knospe1@naver.com>
2023-08-24 16:17:40 +0900

    Feat: add uploadImage api

--- src/api/api.js
+++ src/api/api.js
@@ -8,3 +8,16 @@ export async function getAdminList() {
   const adminList = await get(ref(db, key));
   return adminList.val();
 }
+
+export async function uploadImage(file) {
+  const data = new FormData();
+  data.append('file', file);
+  data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
+
+  const url = process.env.REACT_APP_CLOUDINARY_URL;
+  const imageUrl = await fetch(url, {
+    method: "POST",
+    body: data,
+  }).then((res) => res.json()).then((data) => data.url);
+  return imageUrl;
+}
