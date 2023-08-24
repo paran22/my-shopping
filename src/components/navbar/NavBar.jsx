d6463707df307b81af6ac6275068f4b72325b757
gaeun <knospe1@naver.com>
2023-08-24 09:09:49 +0900

    Refactor: refactor NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,21 +2,31 @@ import React from "react";
 import { useNavigate } from "react-router";
 import { BsCart2, BsFillPencilFill } from "react-icons/bs";
 import { NavIconItem, NavTextItem } from "./NavItem";
-import LoginButton from "./LoginButton";
+import UserInfo from "./UserInfo";
+import { useUserContext } from "../../context/UserContext";
 
 export default function NavBar() {
   const navigate = useNavigate();
+  const { isAuthInfoLoading, isLogin, login, logout } = useUserContext();
   const navigateProductsPage = () => navigate("/products");
   const navigateCartsPage = () => navigate("/carts");
   const navigateProductAddPage = () => navigate("/products/add");
   const cartsIcon = <BsCart2 />;
   const addIcon = <BsFillPencilFill />;
   return (
-    <div className="flex gap-2 items-center">
-      <NavTextItem name="Products" onClick={navigateProductsPage} />
-      <NavIconItem icon={cartsIcon} onClick={navigateCartsPage} />
-      <NavIconItem icon={addIcon} onClick={navigateProductAddPage} />
-      <LoginButton />
-    </div>
+    <>
+      {!isAuthInfoLoading && (
+        <div className="flex gap-2 items-center">
+          <NavTextItem name="Products" onClick={navigateProductsPage} />
+          <NavIconItem icon={cartsIcon} onClick={navigateCartsPage} />
+          <NavIconItem icon={addIcon} onClick={navigateProductAddPage} />
+          <UserInfo />
+          <NavTextItem
+            name={isLogin ? "Logout" : "Login"}
+            onClick={isLogin ? logout : login}
+          />
+        </div>
+      )}
+    </>
   );
 }
