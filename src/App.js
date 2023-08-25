af2064a53812b457c61c1b0af56110337244a8bf
gaeun <knospe1@naver.com>
2023-08-25 14:06:23 +0900

    Config: add react-query

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Outlet } from 'react-router';
 import Header from './components/Header';
 import { AuthContextProvider } from './context/AuthContext';
 
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div>
       <AuthContextProvider>
-        <Header />
-        <Outlet />
+        <QueryClientProvider client={queryClient}>
+          <Header />
+          <Outlet />
+        </QueryClientProvider>
       </AuthContextProvider>
-    </div>
+    </div >
   );
 }
 
